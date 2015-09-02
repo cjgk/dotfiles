034fdeb2f09fbf8e278a08ad407f2a55d4de042b
Jonas Karlsson <jonas.karlsson@stim.se>
2015-09-02 12:34:37 +0200

    fixed bad path

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 
 ln -sf $(pwd)/vim ~/.vim
 ln -sf $(pwd)/vim/vimrc ~/.vimrc
-ln -sf $(pwd)/bash/profile ~/.profile
+ln -sf $(pwd)/shell/profile ~/.profile
 ln -sf $(pwd)/shell/bashrc ~/.bashrc
 ln -sf $(pwd)/git/gitconfig ~/.gitconfig
 ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
