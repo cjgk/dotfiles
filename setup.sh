138ac35e33977d4a5ffb6deec98613998831b4fe
Jonas Karlsson <jonas.karlsson@stim.se>
2015-09-02 12:09:19 +0200

    separate vundle and vim rc

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 ln -sf $(pwd)/vim ~/.vim
-ln -sf $(pwd)/vim/.vimrc ~/.vimrc
+ln -sf $(pwd)/vim/vimrc ~/.vimrc
 ln -sf $(pwd)/bash/profile ~/.profile
 ln -sf $(pwd)/shell/bashrc ~/.bashrc
 ln -sf $(pwd)/git/gitconfig ~/.gitconfig
 ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
+
+if [ -e ~/.vim/vundlerc ]; then
+    vim -u ~/.vim/vundlerc +PluginInstall +qall
+fi
 
 echo "DONE"
