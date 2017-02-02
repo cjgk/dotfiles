86f314d60158d2bcdd45dadec4698ff2e56dd7c6
Jonas Hässel <i@cjkg.me>
2017-02-02 02:31:14 +0100

    docker + tmux

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -sf $(pwd)/shell/profile ~/.profile
 ln -sf $(pwd)/shell/bashrc ~/.bashrc
 ln -sf $(pwd)/git/gitconfig ~/.gitconfig
 ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/docker ~/.docker
 
 if [ -e ~/.vim/vundlerc ]; then
     vim -u ~/.vim/vundlerc +PluginInstall +qall
